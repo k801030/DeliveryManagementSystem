be50939e942202b5da958bbc236eb08b115641cc
k801030 <kk801030@gmail.com>
2015-05-06 13:58:58 +0800

    success add data to db

--- public/js/front.js
+++ public/js/front.js
@@ -1,5 +1,11 @@
 angular.module('front', [])
 
-controller('tableView', ["$scope", "$http", function($scope, $http) {
-	var data = $http.get('data/orders.js')
+.controller('tableView', ["$scope", "$http", function($scope, $http) {
+	var data = $http.get('/api/order/all')
+		.success(function(data, status) {
+			console.log(data);
+		})
+		.error(function(data, status) {
+			console.log("error: "+ status);
+		})
 }])
--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,25 @@ var order = mongoose.model('Order');
 
 /* save to db */
 router.post('/save', function(req, res, next) {
- 	console.log('fi');
- 	console.log(req.body);
- 	//var instance = new order()
- 	//db.save();
+	/*
+ 	console.log('save operation');
+ 	objs = req.body;
+ 	var instance;
+ 	for(var i=0; i<objs.length; i++){
+ 		//string = JSON.stringify(objs[i])
+ 		instance = new order(objs[i]);
+ 		instance.save();
+ 	}
+ 	*/
  	res.end();
 });
 
+router.get('/order/all', function(req, res, next) {
+
+	order.find().exec(function(err, docs) {
+		res.write(docs);
+		res.end();
+	});
+	
+})
 module.exports = router;
