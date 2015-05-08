9d00be84b559ebe5096fc2ffb818beb1f185f5ff
k801030 <kk801030@gmail.com>
2015-05-08 13:53:22 +0800

    orderby: seq

--- public/js/back.js
+++ public/js/back.js
@@ -1,11 +1,18 @@
 angular.module('back', ['keyValue'])
 
-.controller('tableView', ["$scope", "$http", function($scope, $http) {
+.controller('tableView', ["$scope", "$http", 'keyValue', function($scope, $http, keyValue) {
 	$scope.orders = [];
+	$scope.key = keyValue;
 
 	var socket = io('http://140.112.107.72:1234/');
 	socket.on('order', function(data){
 		//$scope.orders = data;
+		load();
+	});
+
+	load();
+	
+	function load() {
 		$http.get('/api/order/status/1')
 			.success(function(data, status) {
 				//console.log(data);
@@ -14,5 +21,6 @@ angular.module('back', ['keyValue'])
 			.error(function(data, status) {
 				console.log("error: "+ status);
 			});
-	});
+	}
 }])
+
